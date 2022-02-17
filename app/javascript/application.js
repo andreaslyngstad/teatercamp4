d907465b95567b2d49f5bd08346c016ebcc3d033
Andreas Lyngstad <andreaslyngstad@gmail.com>
2022-02-17 23:13:06 +0100

    lots!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= link_tree ../assets/images
 //= link_directory ../assets/stylesheets .css
-
+//= link_tree ../assets/JavaScripts
 import "trix"
 import "@rails/actiontext"
